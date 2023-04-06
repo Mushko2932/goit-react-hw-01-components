4625d00f9f707b0a2021032b0190cbe3358acbcb
Mykhailo Kuzmenko <103496282+Mushko2932@users.noreply.github.com>
2023-04-06 17:18:15 +0300

    task1(markup)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import { Profile } from './Profile';
+import user from 'user.json';
+
 export const App = () => {
   return (
     <div
@@ -6,11 +9,17 @@ export const App = () => {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        fontSize: 20,
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
